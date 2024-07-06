242d1856e2325c99f95c01df92392aaa3a430fbd
Suboor Khan <suboorkhan@krapton.com>
2024-07-07 04:44:19 +0530

    c

--- src/core/config/firebase-config.ts
+++ src/core/config/firebase-config.ts
@@ -1,3 +1,9 @@
 export const firebase_config = {
-
+  apiKey: "AIzaSyAgWFIU-NGMUV5wARG7vSRjhrL4psi_icA",
+  authDomain: "personal-36b0f.firebaseapp.com",
+  projectId: "personal-36b0f",
+  storageBucket: "personal-36b0f.appspot.com",
+  messagingSenderId: "1026764985171",
+  appId: "1:1026764985171:web:49f31124048213714d47a6",
+  measurementId: "G-LR5NP550VH"
 };
